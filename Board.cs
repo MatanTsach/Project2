fad73fc54d7005a79019d7ec41d5b20b500e5d27
Dolev <dolevg621@gmail.com>
2023-05-16 16:36:51 +0300

    Board Function V1

---
+++ Board.cs
@@ -0,0 +1,56 @@
+class Board
+{
+    private string[,] matrix;
+    private int m_matrixSize;
+
+
+    public Board(int size)
+    {
+        m_matrixSize = size;
+        matrix = new string[size,size];
+        for (int row = 0; row < size; row++)
+            {
+                for (int column = 0; column < size; column++)
+                {
+                    matrix[row, column] = "   "; 
+                }
+            }
+    }
+    public void refreshMatrix()
+    {
+        string seperatorLine =" ";
+        for (int i = 0 ; i <=(m_matrixSize * 4); i++)
+        {
+            seperatorLine += "=";
+        }
+        Console.Write("  ");
+        for (int i = 0 ; i < m_matrixSize ; i++)
+        {
+            Console.Write( i +1 + "   ");
+        }
+        Console.WriteLine("");
+        for (int row = 0; row < m_matrixSize ; row++)
+            {
+                for (int column = 0; column < m_matrixSize ; column++)
+                {
+                    if (column == 0)
+                        Console.Write(row+1 + "|" + matrix[row,column]);
+                    else
+                    {
+                        if(column == (m_matrixSize-1))
+                        Console.WriteLine("|" + matrix[row,column] + "|");
+                        else
+                        Console.Write("|" + matrix[row,column]);
+                    }
+                }
+                Console.WriteLine(seperatorLine);
+            }
+    }
+
+    public string[,] getBoard()
+    {
+        return matrix;
+    }
+    
+    
+}
--- Game.cs
+++ Game.cs
@@ -1,1 +1,51 @@
 ﻿
+class Game
+{
+    public static void Main()
+    {
+        int boardSize = getBoardSize();
+        bool gameMode = getMuldiOrSingle();
+        //GameManager gameManager = new GameManager(boardSize,gameMode);
+        //gameManager.startGame();
+        Board b = new Board(boardSize);
+        b.refreshMatrix();
+    }
+
+    private static int getBoardSize() {
+        Console.WriteLine("Welcome to Matan and Dolev's Tic Tac Toe game");
+        Console.WriteLine("Please enter a board size:");
+        int sizeInputNum = 0;
+        while(true)
+        {
+            if(int.TryParse(Console.ReadLine(), out sizeInputNum))
+            {
+                if (sizeInputNum >=3 && sizeInputNum <=9)
+                {
+                    break;
+                }
+                Console.WriteLine("INVALID INPUT, PLEASE ENTER AGAIN");
+            }
+        }
+        return sizeInputNum;
+    }
+    // return true if multiplayer false if single
+    private static bool getMuldiOrSingle()
+    {
+        Console.WriteLine("Do you want to play in single or multi player mode?");
+        Console.WriteLine("press S form single or M for multi");
+        string modeInput;
+        while(true)
+        {
+            modeInput = Console.ReadLine();
+            if(modeInput == "S" || modeInput == "M")
+            {   
+                break;
+            }
+            else
+            {
+                Console.WriteLine("INVALID INPUT, PLEASE ENTER AGAIN");
+            }
+        }
+        return modeInput == "M" ? true : false;
+    }
+}
