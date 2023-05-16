15d6d3338fb3e12cf5c6859bec10173c7da6ac58
Matan Tsach <matan.tsach@gmail.com>
2023-05-16 18:43:07 +0300

    Change to GameEngine

--- GameEngine.cs
+++ GameEngine.cs
@@ -1,26 +1,32 @@
 using System.Text.RegularExpressions;
 public abstract class GameEngine {
-    private GameState m_gameState;
-    private Board m_gameBoard;
-    private readonly string[] m_gameMarks = { "X", "O" };
+    protected GameState m_gameState;
+    protected Board m_gameBoard;
+    protected readonly string[] m_gameMarks = { "X", "O" };
 
-    public GameEngine(BoardUtils i_gameBoard)
+    protected int m_playerIdTurn = 1;
+    public GameEngine(Board i_gameBoard)
     {
         m_gameBoard = i_gameBoard;
         m_gameState = GameState.GAME_CONTINUE;
     }
     public abstract void playTurn();
-    private (int, int) getUserInput(int i_playerId)
+    protected (int, int) getUserInput()
     {
-        string input;
-        string pattern = @"\(([1-9]), ([1-9])\)";
+        Match match;
         int gameBoardSize = m_gameBoard.getBoardSize();
         int x = 0;
         int y = 0;
-        Match match;
+        string input;
+        string pattern = @"\(([1-9]), ([1-9])\)";
+        string getCoordsMessage =$"[Player{m_playerIdTurn}] Please enter a cell to insert {m_gameMarks[m_playerIdTurn - 1]} in the format (x, y)";
+        string cellAlreadyTakenMessage = "This cell is already taken. Please choose another cell.";
+        string incorrectEntryMessage = $"Please enter the row and column in the correct format.\n" + 
+                $"Please note: X and Y must be in the bounds of the board. 1 <= x,y <= {gameBoardSize}";
+        
         while (true)
         {
-            Console.WriteLine($"[Player{i_playerId}] Please enter a cell to insert {m_gameMarks[i_playerId - 1]} in the format (x, y)");
+            Console.WriteLine(getCoordsMessage);
             input = Console.ReadLine();
             if (input == "Q")
             {
@@ -33,11 +39,15 @@ public abstract class GameEngine {
                 x = int.Parse(match.Groups[1].Value);
                 y = int.Parse(match.Groups[2].Value);
                 if(!(x < 1 || y < 1 || x > gameBoardSize || y > gameBoardSize)) {
+                    if(!(m_gameBoard.isCellAvailable(x - 1, y - 1)))
+                    {
+                       Console.WriteLine(cellAlreadyTakenMessage);
+                       continue;
+                    }
                     break;
                 }
             }
-            Console.WriteLine($"Please enter the row and column in the correct format.\n" + 
-                $"Please note: X and Y must be in the bounds of the board. 1 <= x,y <= {gameBoardSize}");
+            Console.WriteLine(incorrectEntryMessage);
         }
         return (x, y);
     }
