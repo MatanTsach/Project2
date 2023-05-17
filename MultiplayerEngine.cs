96a17fc19348faddee0dce6b9f3e793f161a2c2b
Matan Tsach <matan.tsach@gmail.com>
2023-05-17 10:33:42 +0300

    Implemented MultiPlayerEngine

--- MultiplayerEngine.cs
+++ MultiplayerEngine.cs
@@ -1,29 +1,24 @@
-using System.Text.RegularExpressions;
-public class MultiplayerEngine : GameEngine
+public class MultiPlayerEngine : GameEngine
 {
-    public MultiplayerEngine(Board i_gameBoard)
+    public MultiPlayerEngine(Board i_gameBoard)
         : base(i_gameBoard)
     {
-        
+
     }
 
     public override void playTurn()
     {
-        (int player1_X, int player2_Y) = getUserInput(1);
-        if (!(m_gameState == GameState.GAME_END))
-        {   
-
+        string prefix = $"[Player{m_playerIdTurn}]";
+        string sign = m_gameMarks[m_playerIdTurn - 1];
+        (int row, int col) = ConsoleUtils.requestUserCellInput(m_gameBoard, prefix, sign, out m_gameState);
+        if(!(m_gameState == GameState.GAME_END))
+        {
+            m_gameBoard.updateBoard(row, col, sign);
+            if(BoardUtils.checkWin(m_gameBoard.getBoard(), sign))
+            {
+                m_gameState = m_playerIdTurn == 1 ? GameState.GAME_PLAYER1_WIN : GameState.GAME_PLAYER2_WIN;
+            }
         }
-    }
-
-    public GameState getGameState()
-    {
-        return 0;
-    }
-
-
-    private bool evaluateBoard()
-    {
-
+        m_playerIdTurn = m_playerIdTurn == 1 ? 2 : 1;
     }
 }
